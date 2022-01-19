253e55128227a46bb23faea13021e2a9c8756dec
maringernina <nina.maringer01@htl-salzburg.ac.at>
2022-01-19 22:39:47 +0100

    clean up directory

--- CandyCatcher-nima/Assets/Scripts/PlayerMovement.cs
+++ CandyCatcher-nima/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public Vector2 direction;
+    public Rigidbody2D player;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     if (Input.GetKeyDown(leftKey))
+        {
+
+            player.AddForce((direction*-1)*speed);
+            Debug.Log("go left");
+        }
+
+        if (Input.GetKeyDown(rightKey))
+        {
+            player.AddForce(direction*speed);
+            Debug.Log("go right");
+        }
     }
 }
