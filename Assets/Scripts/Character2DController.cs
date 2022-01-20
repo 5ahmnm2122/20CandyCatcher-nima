acc4410418df141e6e16c5076914b19de4af5548
maringernina <nina.maringer01@htl-salzburg.ac.at>
2022-01-20 11:42:46 +0100

    add CharacterController Script

---
+++ Assets/Scripts/Character2DController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Character2DController : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+        
+    }
+}
